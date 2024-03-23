4a0b700519c5e816a77bed299f0e2d58f1488e71
Antonimason <sba23108@student.cct.ie>
2024-03-23 10:23:54 +0000

    Project at 78% done
    admin and office class commented and fixed

--- src/Users/Office.java
+++ src/Users/Office.java
@@ -3,47 +3,58 @@ package Users;
 import dbConnector.dbConnector;
 
 public class Office {
+    
     private String name;
     private String username;
     private String password;
     
+    //Default Office constructor
     public Office() {
         this.name = "Office";
         this.username = "office";
         this.password = "java";
     }
-    
+    //Office constructor with parameters for name, username and password
     public Office(String name, String username, String password) {
         this.name = name;
         this.username = username;
         this.password = password;
         System.out.println("Your Office Account has been successfully created");
     }
-
+    
+    //Method Getter for office name
     public String getName() {
         return name;
     }
-
+    
+    //Method Getter for office username
     public String getUsername() {
         return username;
     }
-
+    
+    //Method Getter for office password
     public String getPassword() {
         return password;
     }
-    
+    //Method Setter for office name
     public void setName(String name) {
-        this.name = name;
-        System.out.println("Office Account name has been successfully changed");
+        //it must be longer than 3 letters
+        if(name.length() >= 3){
+            this.name = name;
+            System.out.println("Office Account name has been successfully changed");
+        }
     }
+    //Method Setter for office username
     public void setUsername(String username) {
+        //it must be longer than 3 letters
         if(username.length() >= 4) {
             this.username = username;
             System.out.println("Your username has been successfully changed");
         }
     }
-
+    //Method Setter for office password
     public void setPassword(String password) {
+        //it must be longer than 7 letters
         if(password.length() >= 8) {
             this.password = password;
             System.out.println("Your passsword has been successfully changed");
